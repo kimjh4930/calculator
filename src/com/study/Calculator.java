5441f6eaedf5d6d649cc14c6d8b26eaccc69d012
JunhaKim <kimjh4930@gmail.com>
2015-01-03 12:34:16 +0900

    초기등록

---
+++ src/com/study/Calculator.java
@@ -0,0 +1,24 @@
+package com.study;
+
+public class Calculator {
+	
+	private int num1 = 5;
+	private int num2 = 4;
+
+	public int add() {
+		return num1 + num2;
+	}
+
+	public int subtract() {
+		return num1 - num2;
+	}
+
+	public int multiply() {
+		return num1 * num2;
+	}
+
+	public double divide() {
+		return num1 / num2;
+	}
+
+}
---
+++ src/com/study/Main.java
@@ -0,0 +1,15 @@
+package com.study;
+
+public class Main {
+	public static void main(String args[]) {
+		
+		Calculator cal = new Calculator();
+		
+		System.out.println(cal.add());
+		System.out.println(cal.subtract());
+		System.out.println(cal.multiply());
+		System.out.println(cal.divide());
+		
+	}
+
+}
